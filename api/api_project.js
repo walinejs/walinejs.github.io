25964d805eeae94e8edf6af5984e60e6a4a632a7
lizheming <i@imnerd.org>
2021-09-20 16:52:09 +0000

    Deploying to gh-pages from @ walinejs/waline@27cc2cde429d90f8bdfacd59364db797d284c3b5 🚀

--- api/api_project.js
+++ api/api_project.js
@@ -1,14 +1,14 @@
 define({
   "title": "Waline API Documentation",
   "name": "waline",
-  "version": "0.0.0",
+  "version": "0.0.1",
   "description": "",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-20T16:44:09.403Z",
+    "time": "2021-09-20T16:52:05.953Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
