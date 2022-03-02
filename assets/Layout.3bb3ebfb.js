293646ea0b9dbf7aade0ada3c64fa4bee6ec38b8
lizheming <i@imnerd.org>
2022-03-02 15:19:55 +0000

    Deploying to gh-pages from @ walinejs/waline@f25c2b423317f78df4297d3929a3a9f39f0969e2 🚀

---
+++ assets/Layout.3bb3ebfb.js
@@ -0,0 +1,1 @@
+import{g as r,n as m,j as a,r as e,p as n,q as s}from"./app.5bddcad1.js";import{S as p}from"./SkipLink.6e529fc7.js";var i=r({name:"Layout",setup(){const o=s(),t=m();return()=>[a(p),a(e("CommonWrapper"),()=>t.value.home?a(e("HomePage")):a(n,()=>a(e("NormalPage"),{key:o.value.path})))]}});export{i as default};
