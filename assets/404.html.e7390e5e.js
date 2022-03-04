266c23f4cc99298530addf0e579b13fe9887e920
lizheming <i@imnerd.org>
2022-03-04 00:28:27 +0000

    Deploying to gh-pages from @ walinejs/waline@91f9c197b99016b62d00979c33bea2087eec3cf0 🚀

---
+++ assets/404.html.e7390e5e.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.eec9a043.js";const _={};function e(t,c){return null}var a=r(_,[["render",e]]);export{a as default};
--- assets/intro.html.8a317589.js
+++
@@ -1,7 +0,0 @@
-const e={key:"v-396f59cd",path:"/en/advanced/intro.html",title:"Intro",lang:"en-US",frontmatter:{title:"Intro",icon:"intro",summary:`Waline
-A simple comment system with backend support fork from Valine.
-
-`,head:[["meta",{property:"og:url",content:"https://waline.js.org/en/advanced/intro.html"}],["meta",{property:"og:title",content:"Intro"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{name:"twitter:card",content:"summary_large_image"}]]},excerpt:`<div class="custom-container info"><p class="custom-container-title">Waline</p>
-<p>A simple comment system with backend support fork from <a href="https://valine.js.org" target="_blank" rel="noopener noreferrer">Valine<ExternalLinkIcon/></a>.</p>
-</div>
-`,headers:[{level:2,title:"Feature",slug:"feature",children:[]},{level:2,title:"Todo",slug:"todo",children:[]}],git:{createdTime:1646324259e3,updatedTime:1646324259e3,contributors:[{name:"Mr.Hope",email:"zhangbowang1998@gmail.com",commits:1}]},readingTime:{minutes:.63,words:189},filePathRelative:"en/advanced/intro.md"};export{e as data};
