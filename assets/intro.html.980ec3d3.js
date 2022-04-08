c5fef75de6de11e53a945807b1436d298c837221
lizheming <i@imnerd.org>
2022-04-08 15:19:40 +0000

    Deploying to gh-pages from @ walinejs/waline@a1cba23c5b2d8dff40b9d826b0a5b1a751691cf2 🚀

---
+++ assets/intro.html.980ec3d3.js
@@ -0,0 +1,7 @@
+const e={key:"v-396f59cd",path:"/en/advanced/intro.html",title:"Intro",lang:"en-US",frontmatter:{title:"Intro",icon:"intro",summary:`Waline
+A simple comment system with backend support fork from Valine.
+
+`,head:[["meta",{property:"og:url",content:"https://waline.js.org/en/advanced/intro.html"}],["meta",{property:"og:site_name",content:"Waline"}],["meta",{property:"og:title",content:"Intro"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}]]},excerpt:`<div class="custom-container info"><p class="custom-container-title">Waline</p>
+<p>A simple comment system with backend support fork from <a href="https://valine.js.org" target="_blank" rel="noopener noreferrer">Valine<ExternalLinkIcon/></a>.</p>
+</div>
+`,headers:[{level:2,title:"Feature",slug:"feature",children:[]},{level:2,title:"Todo",slug:"todo",children:[]}],git:{createdTime:164943109e4,updatedTime:164943109e4,contributors:[{name:"Austin Lee",email:"i@imnerd.org",commits:1}]},readingTime:{minutes:.58,words:174},filePathRelative:"en/advanced/intro.md"};export{e as data};
