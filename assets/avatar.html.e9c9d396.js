adaed7c663093f796136207f43b4088e51563cc6
lizheming <i@imnerd.org>
2022-04-11 01:58:23 +0000

    Deploying to gh-pages from @ walinejs/waline@2596213d4bf5f32a29d22bfcf3d8d94a8ff95f82 🚀

---
+++ assets/avatar.html.e9c9d396.js
@@ -0,0 +1,13 @@
+const e={key:"v-e64f5648",path:"/en/guide/client/avatar.html",title:"Avatar Configuration",lang:"en-US",frontmatter:{title:"Avatar Configuration",icon:"avatar",summary:`Obsolete
+We recommand you to use lastest server and config it with AVATAR_PROXY.
+
+Waline currently uses [Libravatar][1] as the comment list avatar.
+Libravatar is a free, open-source avatar provider features federated hosting and Gravatar-compatible APIs.
+Users should log in or register by themselves [Libravatar][1], then set or modify their avatar. When commenting, just leave the email address you used when registering in [Libravatar][1]. Note that if an image is not found in the [Libravatar][1] database and the hash algorithm used was MD5, then Libravatar will first redirect to [Gravatar][2] in case the image exists there.
+`,head:[["meta",{property:"og:url",content:"https://waline.js.org/en/guide/client/avatar.html"}],["meta",{property:"og:site_name",content:"Waline"}],["meta",{property:"og:title",content:"Avatar Configuration"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:image",content:"https://waline.js.org/"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{name:"twitter:card",content:"summary_large_image"}],["meta",{name:"twitter:image:alt",content:"Avatar Configuration"}]]},excerpt:`<div class="custom-container warning"><p class="custom-container-title">Obsolete</p>
+<p>We recommand you to use lastest server and config it with <code>AVATAR_PROXY</code>.</p>
+</div>
+<p>Waline currently uses [Libravatar][1] as the comment list avatar.</p>
+<p>Libravatar is a free, open-source avatar provider features federated hosting and Gravatar-compatible APIs.</p>
+<p>Users should log in or register by themselves [Libravatar][1], then set or modify their avatar. When commenting, just leave the email address you used when registering in [Libravatar][1]. Note that if an image is not found in the [Libravatar][1] database and the hash algorithm used was MD5, then Libravatar will first redirect to [Gravatar][2] in case the image exists there.</p>
+`,headers:[{level:2,title:"Options",slug:"options",children:[]},{level:2,title:"Available Values",slug:"available-values",children:[]},{level:2,title:"Attentions",slug:"attentions",children:[]}],git:{createdTime:1649642137e3,updatedTime:1649642137e3,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},readingTime:{minutes:1.29,words:386},filePathRelative:"en/guide/client/avatar.md"};export{e as data};
