aad575f0df39d7b51f385af5566010dff709cbd5
lizheming <i@imnerd.org>
2022-05-02 03:41:54 +0000

    Deploying to gh-pages from @ walinejs/waline@cf42fd70edc14f4a494f5ff71758bf16d1d4ba43 🚀

--- assets/intro.html.9a7310f1.js
+++
@@ -1,7 +0,0 @@
-const e={key:"v-396f59cd",path:"/en/advanced/intro.html",title:"Intro",lang:"en-US",frontmatter:{title:"Intro",icon:"intro",summary:`Waline
-A simple comment system with backend support fork from Valine.
-
-`,head:[["meta",{property:"og:url",content:"https://waline.js.org/en/advanced/intro.html"}],["meta",{property:"og:site_name",content:"Waline"}],["meta",{property:"og:title",content:"Intro"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:updated_time",content:"2022-05-02T00:46:43.000Z"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:modified_time",content:"2022-05-02T00:46:43.000Z"}]]},excerpt:`<div class="custom-container info"><p class="custom-container-title">Waline</p>
-<p>A simple comment system with backend support fork from <a href="https://valine.js.org" target="_blank" rel="noopener noreferrer">Valine<ExternalLinkIcon/></a>.</p>
-</div>
-`,headers:[{level:2,title:"Feature",slug:"feature",children:[]},{level:2,title:"Todo",slug:"todo",children:[]}],git:{createdTime:1651452403e3,updatedTime:1651452403e3,contributors:[{name:"dependabot[bot]",email:"49699333+dependabot[bot]@users.noreply.github.com",commits:1}]},readingTime:{minutes:.58,words:174},filePathRelative:"en/advanced/intro.md"};export{e as data};
