c0eddfb60e2c17c40e661a1d4f213ec87970cc7c
Mister-Hope <mister-hope@outlook.com>
2024-09-08 13:43:13 +0000

    Deploying to gh-pages from @ walinejs/waline@d60df13c9d87cd67f0cbf8aa4388a9baef08b494 🚀

---
+++ assets/vidstack-C-clE4br-CBTEhdkw.js
@@ -0,0 +1,1 @@
+import{bj as i,aq as s}from"./app-BsgkWtVr.js";class n{constructor(a){this.La=a}Xa(){i(this.ya)&&this.fg()}$(){s(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{n as R};
